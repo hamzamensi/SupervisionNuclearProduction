97105d2a217e20846d41a9fd2bff58c240466526
mensi.hamza1994@gmail.com <mensi.hamza1994@gmail.com>
2023-05-20 22:35:56 +0200

    add real time page

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # SupervisionNuclearProduction
+
+Pré-requis:
+- Il faut avoir une version python 3.9.
+
+Installation:
+- cloner le projet en local.
+- python -m venv [nom de virtual env]
+- pip install -r requiremnts.txt
+- python app.py
+
+Informations utiles: 
+- http://localhost:5000/ : une route pour accéder à une page qui affiche un barplot de la somme de la production infrajournalière par heure
+du jour sur la période 01/12/2022 à 10/12/2022.
+- http://localhost:5000/realtime: une route pour accéder à ue page qui affiche en temps réel sur une période hebdomadaire
+glissante.
--- tests/manual/test_utils.py
+++ tests/manual/test_utils.py
@@ -1,4 +1,5 @@
 import os
+import random
 from datetime import datetime, timedelta
 from unittest import TestCase
 
@@ -8,8 +9,8 @@ from core.utils import get_data
 class Test(TestCase):
     def test_get_data(self):
         # normal use case
-        start_date = '2022-12-01T00:00:00+01:00'
-        end_date = '2022-12-11T00:00:00+01:00'
+        start_date = '2023-01-01T00:00:00+01:00'
+        end_date = '2023-01-11T00:00:00+01:00'
         resutl_df = get_data(start_date, end_date)
         start_time_object = datetime.strptime(start_date, '%Y-%m-%dT%H:%M:%S%z')
         end_time_object = datetime.strptime(end_date, '%Y-%m-%dT%H:%M:%S%z')
@@ -18,13 +19,17 @@ class Test(TestCase):
         self.assertEquals(len(resutl_df), diff)
         self.assertEquals(resutl_df['start_date'].min(), start_date)
         self.assertEquals(resutl_df['start_date'].max(), (end_time_object + timedelta(hours=-1)).isoformat())
-
+        # take random hour and test if we have data for this hour
+        random_day = random.randint(1, 11)
+        random_hour = random.randint(0, 24)
+        random_date = datetime(2023, 1, random_day, random_hour).astimezone().isoformat(sep="T", timespec="seconds")
+        random_date_df = resutl_df[resutl_df['start_date'] == random_date]
+        self.assertGreater(len(random_date_df), 0)
         # date format is not correct
         start_date = '2022-12-01T00:00:00+01:00'
         end_date = '2022-12-11T00:00:0001:00'
         with self.assertRaises(ValueError):
             get_data(start_date, end_date)
-
 
         # token is not correct
         start_date = '2022-12-01T00:00:00+01:00'
@@ -33,4 +38,5 @@ class Test(TestCase):
         with self.assertRaises(Exception):
             resutl_df = get_data(start_date, end_date)
 
+        #
 
